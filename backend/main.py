efbc8a1b38748f2159c88fa371819670017590ff
sofeamza <sofeamza@gmail.com>
2026-05-11 19:45:02 +0800

    cleanup

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from config import users_col, profiles_col, scores_col, explanations_col, db
-from routes.auth import router as auth_router
+from config import users_col, profiles_col, scores_col, explanations_col
 
-app = FastAPI()
+from routes.auth import router as auth_router
+from routes.score import router as score_router
+from routes.simulation import router as simulation_router
+from routes.data import router as data_router
+
+app = FastAPI(title="GigCredit API")
+
 app.include_router(auth_router)
+app.include_router(score_router)
+app.include_router(simulation_router)
+app.include_router(data_router)
+
 
 @app.on_event("startup")
 def create_indexes():
@@ -14,58 +23,7 @@ def create_indexes():
 
     print("Indexes created successfully")
 
-@app.get("/test-db")
-def test_db():
-    return {"collections": db.list_collection_names()}
 
-from models.profile import Profile
-
-@app.post("/test-profile")
-def test_profile(profile: Profile):
-    return {"message": "Valid!", "data": profile}
-
-from models.profile import Profile
-from services.scoring_engine import get_score
-
-
-
-from services.scoring_engine import get_score
-from services.shap_explainer import explain_ml
-
-@app.post("/calculate-score")
-def calculate_score(profile: Profile):
-
-    result = get_score(profile)
-
-    if result["model_used"] == "ml":
-        explanation = explain_ml(profile)
-    else:
-        explanation = ["Using safe baseline model due to instability in ML prediction"]
-
-    return {
-        "score": result["score"],
-        "model_used": result["model_used"],
-        "explanation": explanation
-    }
-
-from fastapi import Depends
-from services.auth_service import get_current_user
-
-@app.get("/protected-test")
-def protected_test(current_user: dict = Depends(get_current_user)):
-    return {
-        "message": "You are authenticated",
-        "user": current_user
-    }
-
-from routes.score import router as score_router
-
-app.include_router(score_router)
-
-from routes.simulation import router as simulation_router
-
-app.include_router(simulation_router)
-
-from routes.data import router as data_router
-
-app.include_router(data_router)
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
